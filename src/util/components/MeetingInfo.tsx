bb1297c5258a1f536b5825edcd3d64e46a9906c6
ted <tmcculloch@g.harvard.edu>
2025-02-03 15:26:44 -0500

    made location more specific; using Text object

--- src/util/components/MeetingInfo.tsx
+++ src/util/components/MeetingInfo.tsx
@@ -1,3 +1,4 @@
+import {Text} from "grommet"
 export function MeetingInfo(){
-    return <span>GAC meetings are held <b>every Tuesday at 4-5PM</b> in the SEC OMPP Conference Room.</span>
+    return <Text size="1.6rem">GAC meetings are held <b>every Tuesday at 4-5PM</b> in the <b>SEC 1.312</b> OMPP Conference Room.</Text>
 }
